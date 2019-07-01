1c66b2095c36926a95772c6b51bb88eed6438101
arju88nair <arju88nair@gmail.com>
2019-07-01 14:48:26 +0545

    url check

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import Snackbar from "@material-ui/core/Snackbar";
 import MySnackbarContent from "./utils/snackbar";
 import Chip from '@material-ui/core/Chip';
 import Assignment from '@material-ui/icons/Assignment';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { ClipLoader } from 'react-spinners';
 import { css } from '@emotion/core';
 
@@ -33,10 +33,10 @@ const styles = theme => ({
   },
   chip: {
     margin: theme.spacing.unit,
-    color:'black',
-    padding:14,
-    fontWeight:'bold',
-    backgroundColor:'lightgrey'
+    color: 'black',
+    padding: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'lightgrey'
   },
   button: {
     margin: theme.spacing.unit,
@@ -84,27 +84,27 @@ const styles = theme => ({
 
 
 class App extends Component {
-  
+
   constructor(props) {
     super(props);
-    this.state = { url: "", open: false, message: "", variant: "success",chip :false,slug:'',loading: false,disabled:false};
+    this.state = { url: "", open: false, message: "", variant: "success", chip: false, slug: '', loading: false, disabled: false };
     this.keyPress = this.keyPress.bind(this);
   }
 
-   chipClick = () =>{
-    this.setState({copied: true})
+  chipClick = () => {
+    this.setState({ copied: true })
     this.openSnack({ message: "Copied", variant: "success" });
   }
 
   // For calling the API on enter key click
-  keyPress(e){
-    if(e.keyCode === 13){
-      this.setState({ url: e.target.value});
-       this.fetchData()
+  keyPress(e) {
+    if (e.keyCode === 13) {
+      this.setState({ url: e.target.value });
+      this.fetchData()
     }
- }
+  }
 
- // Opening the snack bar
+  // Opening the snack bar
   openSnack(state) {
     this.setState({ open: true, ...state });
   }
@@ -118,12 +118,29 @@ class App extends Component {
   handleChange = name => event => {
     this.setState({ url: event.target.value });
   };
+  
 
+  validURL = (str) => {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
+  }
 
   // API call
   fetchData = () => {
-    this.setState({loading:true,disabled:true})
+    if(!this.validURL(this.state.url))
+    {
+      this.openSnack({ message: "Not a proper url", variant: "error" });
+      return false;
+
+    }
+    this.setState({ loading: true, disabled: true })
     const data = { url: this.state.url };
+    console.log(data)
     fetch("http://127.0.0.1:5000", {
       method: "POST",
       dataType: "JSON",
@@ -139,7 +156,7 @@ class App extends Component {
         if (data.success === false) {
           this.openSnack({ message: data.message, variant: "error" });
         } else {
-          this.setState({slug:`http://127.0.0.1:5000/${data.url}`,chip:true,loading:false,disabled:false})
+          this.setState({ slug: `http://127.0.0.1:5000/${data.url}`, chip: true, loading: false, disabled: false })
         }
         console.log(data);
       })
@@ -148,7 +165,7 @@ class App extends Component {
           message: "Something went wrong. Please try again later",
           variant: "error"
         });
-        this.setState({loading:false,disabled:false})
+        this.setState({ loading: false, disabled: false })
         console.log(error, "catch the hoop");
       });
   };
@@ -157,8 +174,8 @@ class App extends Component {
     const { classes } = this.props;
     return (
       <div className="App-header">
-      <div className='sweet-loading'>
-      </div> 
+        <div className='sweet-loading'>
+        </div>
         <Snackbar
           anchorOrigin={{
             vertical: "bottom",
@@ -216,26 +233,26 @@ class App extends Component {
               </FormControl>
             </Grid>
             <Grid item xs={12} className={classes.container}>
-                  { this.state.chip ? 
-                    <CopyToClipboard text={this.state.slug} 
-                    onCopy={() => this.setState({copied: true})}>
+              {this.state.chip ?
+                <CopyToClipboard text={this.state.slug}
+                  onCopy={() => this.setState({ copied: true })}>
                   <Chip
-                  icon={<Assignment />}
-                  label={this.state.slug}
-                  onClick={this.chipClick}
-                  className={classes.chip}
-                  color="black"
+                    icon={<Assignment />}
+                    label={this.state.slug}
+                    onClick={this.chipClick}
+                    className={classes.chip}
+                    color="black"
                   />
-                  </CopyToClipboard>
-                  : null }
+                </CopyToClipboard>
+                : null}
             </Grid>
-             <ClipLoader
-          css={override}
-          sizeUnit={"px"}
-          size={60}
-          color={'#123abc'}
-          loading={this.state.loading}
-        />
+            <ClipLoader
+              css={override}
+              sizeUnit={"px"}
+              size={60}
+              color={'#123abc'}
+              loading={this.state.loading}
+            />
             <Grid item xs={12} className={classes.container}>
               <Button
                 variant="outlined"
